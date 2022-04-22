c184c6486fa9b63bdd488595979497e479a20861
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2022-04-22 17:03:23 -0300

    chore: create marketing script deploy

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,26 +1,32 @@
 const { merge } = require('webpack-merge')
+const path = require('path')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 
 const commonConfig = require('./webpack.common')
 const packageJson = require('../package.json')
 
-const devConfig = {
-  mode: 'development',
-  devServer: {
-    port: 8080,
-    historyApiFallback: true
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: 'MF_Container',
-      remotes: {
-        marketing: 'MF_Marketing@http://localhost:8081/remoteEntry.js'
-      },
-      shared: {
-        ...packageJson.dependencies
-      }
-    })
-  ]
+module.exports = (env) => { 
+
+  const devConfig = {
+    mode: 'development',
+    devServer: {
+      port: 8080,
+      historyApiFallback: true
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: 'MF_Container',
+        remotes: {
+          marketing: `MF_Marketing@${env.MFS_DOMAIN}:8081/remoteEntry.js`
+        },
+        shared: {
+          ...packageJson.dependencies
+        }
+      })
+    ]
+  }
+  
+  return merge(devConfig, commonConfig)
+
 }
 
-module.exports = merge(devConfig, commonConfig)
--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -4,25 +4,29 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common')
 const packageJson = require('../package.json')
 
-const domain = process.env.PRODUCTION_DOMAIN
+const domain = process.env.MFS_DOMAIN
 
-const prodConfig = {
-  mode: 'production',
-  output: {
-    filename: '[name].[contenthash].js',
-    publicPath: '/container/latest'
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: 'MF_Container',
-      remotes: {
-        marketing: `MF_Marketing@${domain}/marketing/remoteEntry.js`
-      },
-      shared: {
-        ...packageJson.dependencies
-      }
-    })
-  ]
+module.exports = (env) => {
+
+  const prodConfig = {
+    mode: 'production',
+    output: {
+      filename: '[name].[contenthash].js',
+      publicPath: '/container/latest'
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: 'MF_Container',
+        remotes: {
+          marketing: `MF_Marketing@${env.MFS_DOMAIN}/marketing/remoteEntry.js`
+        },
+        shared: {
+          ...packageJson.dependencies
+        }
+      })
+    ]
+  }
+
+  return merge(prodConfig, commonConfig)
+
 }
-
-module.exports = merge(prodConfig, commonConfig)
