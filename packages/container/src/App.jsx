1be174d91ca5ca27643e460731126e68a3a9468a
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2022-04-23 13:33:34 -0300

    fix: adding scoped class name in production on container and marketing projects

--- packages/container/src/App.jsx
+++ packages/container/src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
 import { Header, MarketingApp } from './components'
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co'
+})
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <MarketingApp />
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Header />
+        <MarketingApp />
+      </BrowserRouter>
+    </StylesProvider>
   )
 }
 
--- packages/marketing/src/App.jsx
+++ packages/marketing/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
-import { StylesProvider } from '@material-ui/core/styles'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
 import { Landing, Pricing } from './pages'
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma'
+})
+
 const App = () => {
   return (
-    <StylesProvider>
+    <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <Switch>
             <Route path='/' exact component={Landing} />
